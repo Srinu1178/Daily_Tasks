c793315a6d6831c2fb2a339c25d2be17b0432d45
Srinu1178 <sagasrinu1178@gmail.com>
2026-07-22 18:21:24 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,25 @@
+#Task: Write a 2 real time example on each of, >=,<=
+# >= greater than or equal to
+
+# to check the person is eligible to vote or not
+vote_eligible = int(input("Enter your age: "))
+print(vote_eligible>=18)
+
+# to check the student subject marks pass or fail
+stu_pass = int(input("Enter Your Marks: "))
+print(stu_pass>=35)
+
+#less than or equal to
+# To check the speed limit of the person if it comes true you drive is safe 
+# otherwise you drive not safe
+speed = int(input("Enter your speed: "))
+print(speed<=80)
+
+# to check the weight of the bag if less than 50 kgs it takes free otherwise not
+# to pay extra charge
+weight = int(input("Enter the Weight of the Bag: "))
+print(weight<=50)
+
+
+
+
